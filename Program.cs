2f675463e440ef015dbdcf0ccc2e94f130970add
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2021-07-11 15:29:23 -0700

    Created dice game
    Created a dice game where you roll two dice and you have to keep rolling them until they match.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace My_Awesome_Program_2
     {
         static void Main(string[] args)
         {
+            Random numberGen = new Random();
+
+            int roll = 0;
+            int roll2 = 1;
+            int attempts = 0;
+
+            Console.WriteLine("Press enter to roll the dice");
+
+            while(roll != roll2) {
+                 Console.ReadKey();
+
+                 roll = numberGen.Next(1, 7);
+                 roll2 = numberGen.Next(1, 7);
+
+                 Console.WriteLine("roll 1: " + roll);
+                 Console.WriteLine("roll 2: " + roll2 + "\n");
+                 
+
+                 attempts++;
             
+            }
+
+             Console.WriteLine("It took " + attempts + " to roll two of a kind.");
 
            // Wait Before Closing
            Console.ReadKey();

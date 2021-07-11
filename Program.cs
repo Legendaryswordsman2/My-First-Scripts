4b61a65fb271711cd52c426f3dcf45d64cbde7ba
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2021-07-11 14:59:10 -0700

    Created a cool number generator

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace My_Awesome_Program_2
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("How many cool numbers do you want: ");
+            double count = Convert.ToDouble(Console.ReadLine());
+
+            for (double i = 1; i <= count; i++)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine(result);
+            }
 
            // Wait Before Closing
            Console.ReadKey();

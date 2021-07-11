2a670c891a36d7550bb61bf51fcf0bf570ed2641
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2021-07-11 15:11:11 -0700

    Created a die rolling game

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace My_Awesome_Program_2
     {
         static void Main(string[] args)
         {
+            Random numberGen = new Random();
+
+            int roll = 0;
+            int attempts = 0;
+
+            Console.WriteLine("Press enter to roll the die");
+
+            while(roll != 6) {
+                 Console.ReadKey();
+                 roll = numberGen.Next(1, 7);
+                 Console.WriteLine("You rolled: " + roll);
+                 attempts++;
             
+            }
+
+             Console.WriteLine("It took " + attempts + " attempts to roll a six.");
 
            // Wait Before Closing
            Console.ReadKey();

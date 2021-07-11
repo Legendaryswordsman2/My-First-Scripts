dc454e61ace509ab48f65823a0c0f468393b5ca7
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2021-07-11 13:08:09 -0700

    Created template for coding new projects

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace My_Awesome_Program_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+           // Wait Before Closing
+           Console.ReadKey();
+        }
+    }
+}

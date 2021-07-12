96cb37461ede2ed2e73fbd322d40caffc07e5fe1
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2021-07-11 19:46:51 -0700

    Created a basic list
    created a basic list and modified it a few times

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace My_Awesome_Program_2
 {
@@ -6,7 +7,28 @@ namespace My_Awesome_Program_2
     {
         static void Main(string[] args)
         {
-            
+            List<string> shoppingList = new List<string>();
+
+            shoppingList.Add("Dreams");
+            shoppingList.Add("Miracles");
+            shoppingList.Add("Rainbows");
+            shoppingList.Add("Pony");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+
+            shoppingList.Remove("Dreams");
+            shoppingList.RemoveAt(1);
+
+            Console.WriteLine("-------------------------------------------");
+
+                for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+
 
            // Wait Before Closing
            Console.ReadKey();

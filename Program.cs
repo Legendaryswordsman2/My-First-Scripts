a9c679d39e3c64820c08715eb6ce0e70bde26af3
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2021-07-12 23:59:46 -0700

    Created a alien dating website
    Created a alien dating website to meet aliens

--- Program.cs
+++ Program.cs
@@ -6,10 +6,22 @@ namespace My_Awesome_Program_2
     {
         static void Main(string[] args)
         {
-            
+             MeetAlien();
+             MeetAlien();
+             MeetAlien();
+             MeetAlien();
 
            // Wait Before Closing
            Console.ReadKey();
+        }
+        static void MeetAlien() 
+        {
+            Random numberGen = new Random();
+
+            string name = "X-" + numberGen.Next(10, 9999);
+            int age = numberGen.Next(10, 500);
+
+            Console.WriteLine("Hi, I'm " + name + ".\nI'm " + age + " years old.\nOh, and I'm an alien.\n---------------");
         }
     }
 }
